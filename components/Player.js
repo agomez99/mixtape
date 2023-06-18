f3071c04d919ffc4b33019bc1cb74dfc2812e96e
Austine Gomez <austine.gomez@yahoo.com>
2023-06-17 22:18:13 -0500

    player functioning

---
+++ components/Player.js
@@ -0,0 +1,148 @@
+import React, { useState, useEffect, useRef } from 'react';
+
+const formatTime = (time) => {
+  const minutes = Math.floor(time / 60);
+  const seconds = Math.floor(time % 60).toString().padStart(2, '0');
+  return `${minutes}:${seconds}`;
+};
+
+const tracks = [
+  {
+    title: 'Title 1',
+    artist: 'Artist 1',
+    url: 'https://res.cloudinary.com/dwq30zq8e/video/upload/v1637534672/Nujubes/Nujabes_-_Winter_Lane_Remix_y1cnlf.mp3',
+    duration: '3:30',
+  },
+  {
+    title: 'Title 2',
+    artist: 'Artist 2',
+    url: 'https://res.cloudinary.com/dwq30zq8e/video/upload/v1635017502/musica/9th_Wonder_-_Make_Your_Move_Remix_Instrumental_q1trpk.mp3',
+    duration: '3:30',
+  },
+];
+
+const Player = () => {
+  const audioRef = useRef(null);
+  const [currentTrackIndex, setCurrentTrackIndex] = useState(0);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [progress, setProgress] = useState(0);
+  const [currentTime, setCurrentTime] = useState(0);
+  const [duration, setDuration] = useState(0);
+
+  const currentTrack = tracks[currentTrackIndex];
+
+  const handleToggle = () => {
+    if (isPlaying) {
+
+      audioRef.current.pause();
+    } else {
+      audioRef.current.play();
+
+    }
+    setIsPlaying(!isPlaying);
+  };
+
+  const handleTimeUpdate = () => {
+    const audio = audioRef.current;
+    const currentTime = audio.currentTime;
+    const duration = audio.duration || 0;
+    const progressPercent = (currentTime / duration) * 100;
+    setProgress(progressPercent);
+    setCurrentTime(currentTime);
+    setDuration(duration);
+  };
+
+  const handleTrackEnd = () => {
+    // Check if there are more tracks
+    if (currentTrackIndex < tracks.length - 1) {
+      setCurrentTrackIndex(currentTrackIndex + 1);
+    } else {
+      // All tracks have been played, stop the player
+      audioRef.current.pause();
+      audioRef.current.currentTime = 0;
+      setProgress(0);
+      setIsPlaying(false);
+    }
+  };
+
+const onVolumeChange = (e) => {
+    audioRef.current.volume = e.target.value;
+}
+const handleNextTrack = () => {
+    if (currentTrackIndex < tracks.length - 1) {
+    setIsPlaying(false);
+    audioRef.current.pause();
+      setCurrentTrackIndex(currentTrackIndex + 1);
+    } else {
+      audioRef.current.pause();
+      audioRef.current.currentTime = 0;
+      setProgress(0);
+      setIsPlaying(false);
+    
+  };
+
+
+}
+  const handlePreviousTrack = () => {
+    if (currentTrackIndex > 0) {
+      setCurrentTrackIndex(currentTrackIndex - 1);
+      setIsPlaying(false);
+      audioRef.current.pause()
+    } else {
+      audioRef.current.pause();
+      audioRef.current.currentTime = 0;
+      setProgress(0);
+      setIsPlaying(false);
+    }
+  };
+
+  const handleStop = () => {
+    setIsPlaying(false);
+    audioRef.current.pause();
+    audioRef.current.currentTime = 0;
+    setProgress(0);
+  }
+
+  useEffect(() => {
+    if (isPlaying) {
+      audioRef.current.play();
+    } else {
+      audioRef.current.pause();
+    }
+  }, [isPlaying]);
+
+  useEffect(() => {
+    const audio = new Audio(currentTrack.url);
+    audioRef.current = audio;
+
+    audio.addEventListener('timeupdate', handleTimeUpdate);
+
+    return () => {
+      audio.pause();
+      audio.removeEventListener('timeupdate', handleTimeUpdate);
+    };
+  }, [currentTrack]);
+
+  return (
+    <div>
+
+      <p>{currentTrack.title} - {currentTrack.artist}</p>
+      <audio ref={audioRef} src={currentTrack.url} onTimeUpdate={handleTimeUpdate} onLoadedMetadata={handleTimeUpdate} onEnded={handleTrackEnd} />
+        <label>Volume</label>
+      <input type="range" min="0" max="1" step="0.01" onChange={onVolumeChange} />
+      <div>
+      <button onClick={handleNextTrack}>Next</button>
+      <button onClick={handlePreviousTrack}>Previous</button>
+      <button onClick={handleToggle}>{isPlaying ? 'Pause' : 'Play'}</button>
+      <button onClick={handleStop}>Stop</button>
+      </div>
+      <div>
+        <span >{formatTime(currentTime)}</span> / <span>{formatTime(duration)}</span>
+      </div>
+      <progress value={progress} max='100' />
+
+    </div>
+  );
+};
+
+export default Player;
--- pages/index.js
+++ pages/index.js
@@ -1,52 +1,46 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
-
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Image from 'next/image';
+import Player from '../components/Player';
 export default function Home() {
+  const tracks = [
+    { title: 'Track 1', url: 'https://res.cloudinary.com/dwq30zq8e/video/upload/v1637534672/Nujubes/Nujabes_-_Winter_Lane_Remix_y1cnlf.mp3' },
+    { title: 'Track 2', url: 'https://res.cloudinary.com/dwq30zq8e/video/upload/v1634414042/musica/Nas_Is_Like_-_Phoniks_Remix_on6wdz.mp3' },
+    { title: 'Track 3', url: 'https://res.cloudinary.com/dwq30zq8e/video/upload/v1635017502/musica/9th_Wonder_-_Make_Your_Move_Remix_Instrumental_q1trpk.mp3' },
+  ]
+
+  const handleTimeUpdate = (currentTime) => {
+    console.log('Current time:', currentTime);
+  }
+
+
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-      </Head>
+    <Container>
+      <Row>
+        <Col xs={12} md={8}>
+        <Player 
+        onReady={handleTimeUpdate}
+        onPlay={handleTimeUpdate}
+        onPause={handleTimeUpdate}
+        onEnded={handleTimeUpdate}
+        onSeek={handleTimeUpdate}
+        onSeeked={handleTimeUpdate}
+        onVolumeChange={handleTimeUpdate}
+        onTimeUpdate={handleTimeUpdate}
+        
+         />
+        </Col>
+        <Col xs={6} md={4}>
+        
+        <Image src="/images/cassette.jpg" alt="cassette1" width={300} height={200} />
+        <Image src="/images/cassette2.jpg" alt="cassette1" width={300} height={200} />
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>Mixxtape</h1>
+        </Col>
+      </Row>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a href="https://next.new" target="_blank" rel="noopener noreferrer">
-          Created with&nbsp;<b>next.new</b>&nbsp;⚡️
-        </a>
-      </footer>
-    </div>
+    </Container>
   );
 }
